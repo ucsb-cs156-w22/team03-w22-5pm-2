d423a789469752366e2d25c787c63a1b5e15d417
Alex Lai <a_lai@ucsb.edu>
2022-02-17 23:50:12 -0800

    al - add collegiateSubredditForm

---
+++ frontend/src/fixtures/collegiateSubredditsFixtures.js
@@ -0,0 +1,25 @@
+const collegiateSubredditsFixtures = {
+    oneCollegiateSubreddit: {
+        "id": 1,
+        "name": "test1",
+        "location": "test1",
+        "subreddit": "test1"
+    },
+    twoCollegiateSubreddits: [
+        {
+          "id": 1,
+          "name": "test1",
+          "location": "test1",
+          "subreddit": "test1"
+        },
+        {
+          "id": 2,
+          "name": "test2",
+          "location": "test2",
+          "subreddit": "test2"
+        }
+    ]
+};
+
+
+export { collegiateSubredditsFixtures };
