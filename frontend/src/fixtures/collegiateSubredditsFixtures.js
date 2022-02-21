37c0592d2d3e91183a12b01d5537227427fa0d89
Nicolas Johnson <njohnson@ucsb.edu>
2022-02-20 17:46:05 -0800

    nj - table component and tests for collegiate subreddits finished

--- frontend/src/fixtures/collegiateSubredditsFixtures.js
+++ frontend/src/fixtures/collegiateSubredditsFixtures.js
@@ -1,4 +1,6 @@
 const collegiateSubredditsFixtures = {
+  
+
     oneCollegiateSubreddit: {
         "id": 1,
         "name": "test1",
@@ -18,8 +20,27 @@ const collegiateSubredditsFixtures = {
           "location": "test2",
           "subreddit": "test2"
         }
-    ]
+    ],
+  threeCollegiateSubreddits: [
+      {
+          "id": 1,
+          "name": "Kitten",
+          "location": "KittenVille.org",
+          "subreddit": "Cats"
+      },
+      {
+          "id": 2,
+          "name": "Soccer",
+          "location": "Soccer.org",
+          "subreddit": "Sports"
+      },
+      {
+          "id": 3,
+          "name": "Corn",
+          "location": "CornVille.org",
+          "subreddit": "Farm"
+      }
+  ]
 };
-
 
 export { collegiateSubredditsFixtures };
