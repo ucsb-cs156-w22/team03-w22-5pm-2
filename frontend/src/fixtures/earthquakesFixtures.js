39dad5dacf704a54f4e3973ef067332bb9335d04
ScottUCSB <stg@ucsb.edu>
2022-02-23 12:21:28 -0800

    sg-index page test user issue fixed and braces

--- frontend/src/fixtures/earthquakesFixtures.js
+++ frontend/src/fixtures/earthquakesFixtures.js
@@ -1,4 +1,5 @@
 const earthquakesFixtures = {
+  
     oneEarthquake: [
         {
           "_Id": "6213ea946e8cd25c78e6fba5",
@@ -12,9 +13,37 @@ const earthquakesFixtures = {
           },
           "id": "ci40182560"
         }
-    ]
-      
+    ],
+    twoEarthquake: [
+      {
+        "_Id": "111111111111111111111111",
+        "type": "Feature",
+        "properties": {
+          "mag": "4.87",
+          "place": "11km NW of Santa Paula, CA",
+          "time": "2644539102020",
+          "url": "https://earthquake.usgs.gov/earthquakes/eventpage/ci40182560",
+          "title": "M 3.9 - 11km NW of  tester1, CA"
+        },
+        "id": "ci40182777"
+      }
+  ] ,
+  threeEarthquake: [
+    {
+      "_Id": "222222222222222222222222",
+      "type": "Feature",
+      "properties": {
+        "mag": "4.86",
+        "place": "10km NW of Santa Paula, CA",
+        "time": "3644539102020",
+        "url": "https://earthquake.usgs.gov/earthquakes/eventpage/ci40182560",
+        "title": "M 3.9 - 10km NW of  tester2, CA"
+      },
+      "id": "ci40182666"
+    }
+] 
 };
+      
 
 
 export { earthquakesFixtures };
