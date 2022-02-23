5ff4cfc820fa3b2a3051346b00212185bde88700
ScottUCSB <stg@ucsb.edu>
2022-02-23 11:48:25 -0800

    sg-last push didn't have update

--- frontend/src/tests/pages/Earthquakes/EarthquakesIndexPage.test.js
+++ frontend/src/tests/pages/Earthquakes/EarthquakesIndexPage.test.js
@@ -13,7 +13,6 @@ import { earthquakesFixtures } from "fixtures/earthquakesFixtures";
 
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-//maybe delete next line
 //import { fireEvent, waitFor } from "@testing-library/react";
 
 import mockConsole from "jest-mock-console";
