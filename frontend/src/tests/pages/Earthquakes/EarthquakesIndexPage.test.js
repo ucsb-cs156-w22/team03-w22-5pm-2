e542671970073628fc4850ed4578f21b5aa5967b
ScottUCSB <stg@ucsb.edu>
2022-02-22 17:48:10 -0800

    sg-test page update

--- frontend/src/tests/pages/Earthquakes/EarthquakesIndexPage.test.js
+++ frontend/src/tests/pages/Earthquakes/EarthquakesIndexPage.test.js
@@ -11,7 +11,8 @@ import {earthquakesFixtures} from "fixtures/earthquakesFixtures";
 
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-//import { fireEvent, render, waitFor } from "@testing-library/react";
+//maybe delete next line
+import { fireEvent, render, waitFor } from "@testing-library/react";
 
 import mockConsole from "jest-mock-console";
 
