014259b4d8170f9cc7bc1b9ad7d95419d46b152e
ScottUCSB <stg@ucsb.edu>
2022-02-23 11:24:47 -0800

    sg-eq index test I imported render twice

--- frontend/src/tests/pages/Earthquakes/EarthquakesIndexPage.test.js
+++ frontend/src/tests/pages/Earthquakes/EarthquakesIndexPage.test.js
@@ -12,7 +12,7 @@ import {earthquakesFixtures} from "fixtures/earthquakesFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 //maybe delete next line
-import { fireEvent, render, waitFor } from "@testing-library/react";
+import { fireEvent, waitFor } from "@testing-library/react";
 
 import mockConsole from "jest-mock-console";
 
