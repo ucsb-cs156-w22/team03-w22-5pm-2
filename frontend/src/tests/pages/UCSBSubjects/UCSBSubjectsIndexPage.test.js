9baeef5b89dbb6bef41d387f57ccacf751cf1126
rutvikjha <rutvikjha@umail.ucsb.edu>
2022-02-24 14:13:28 -0800

    rj - implementing delete function and tests

--- frontend/src/tests/pages/UCSBSubjects/UCSBSubjectsIndexPage.test.js
+++ frontend/src/tests/pages/UCSBSubjects/UCSBSubjectsIndexPage.test.js
@@ -135,4 +135,34 @@ describe("UCSBSubjectsIndexPage tests", () => {
         expect(queryByTestId(`${testId}-cell-row-0-col-id`)).not.toBeInTheDocument();
     });
 
+    test("test what happens when you click delete, admin", async () => {
+        setupAdminUser();
+
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/ucsbsubjects/all").reply(200, ucsbSubjectsFixtures.threeDates);
+        axiosMock.onDelete("/api/ucsbsubjects").reply(200, "UCSBSubject with id 1 deleted");
+
+
+        const { getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <UCSBSubjectsIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-id`)).toBeInTheDocument(); });
+
+       expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1"); 
+
+
+        const deleteButton = getByTestId(`${testId}-cell-row-0-col-Delete-button`);
+        expect(deleteButton).toBeInTheDocument();
+       
+        fireEvent.click(deleteButton);
+
+        await waitFor(() => { expect(mockToast).toBeCalledWith("UCSBSubject with id 1 deleted") });
+
+    });
+
 });
