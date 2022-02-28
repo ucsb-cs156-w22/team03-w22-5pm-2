2e9b9877c5a84f139bb0bd648064e293b9a4ddf5
Alex Lai <a_lai@ucsb.edu>
2022-02-27 20:41:22 -0800

    al - fix purge button

--- frontend/src/main/pages/Earthquakes/EarthquakesIndexPage.js
+++ frontend/src/main/pages/Earthquakes/EarthquakesIndexPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useBackend } from 'main/utils/useBackend';
+import { useBackend, useBackendMutation} from 'main/utils/useBackend';
 
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import EarthquakesTable from 'main/components/Earthquakes/EarthquakesTable';
@@ -13,124 +13,47 @@ export default function EarthquakesIndexPage() {
       ["/api/earthquakes/all"], { method: "GET", url: "/api/earthquakes/all" }, []
     );
 
+    const objectToAxiosParams = () => ({
+        url: '/api/earthquakes/purge',
+        method: 'POST',
+      });
+    
+    const deleteMutation = useBackendMutation(
+        objectToAxiosParams,
+        {
+            onSuccess: () => toast('Earthquakes have been purged'),
+        },
+        // Stryker disable next-line all : hard to set up test for caching
+        ['/api/earthquakes/all']
+    );
 
-
+    const deleteCallback = async (data) => {
+        deleteMutation.mutate(data);
+    };
     if(hasRole(currentUser, "ROLE_ADMIN")){
-      return (
-        <BasicLayout>
-          <div className="pt-2">
-            <h1>Earthquakes</h1>
-            <form id="form" action="/api/earthquakes/purge" method="POST">
-              <button name="purge" value="upvote">Purge</button>
-            </form>
-
-            <EarthquakesTable subjects={subjects} currentUser={currentUser} />
-          </div>
-        </BasicLayout>
-      )
+        return (
+            <BasicLayout>
+            <div className="pt-2">
+                <h1>Earthquakes</h1>
+                <EarthquakesTable subjects={subjects} currentUser={currentUser} />
+                <Button variant = 'danger' onClick={deleteCallback}>Purge</Button>
+            </div>
+            </BasicLayout>
+        )
     }
     else{
-  return (
-    <BasicLayout>
-      <div className="pt-2">
-        <h1>Earthquakes</h1>
-        <EarthquakesTable subjects={subjects} currentUser={currentUser} />
-      </div>
-    </BasicLayout>
-  )
-}
+        return (
+            <BasicLayout>
+            <div className="pt-2">
+                <h1>Earthquakes</h1>
+                <EarthquakesTable subjects={subjects} currentUser={currentUser} />
+
+            </div>
+            </BasicLayout>
+        )
+    }
 } 
 
 
 
 
-
-
-/*
-/api/earthquakes/purge
-
-
-
-works but sends to the purge page
-            <form id="form" action="/api/earthquakes/purge" method="POST">
-              <button name="purge" value="upvote">Purge</button>
-            </form>
-
-
-
-
-
-<form action="" method="post">
-    <button name="foo" value="upvote">Buttondisplayed</button>
-</form>
-
-<Button
-                type="submit"
-                data-testid="Purge-submit"
-            >
-                {"Purge"}
-            </Button>
-
-
-
-<Button
-                //variant="Secondary"
-                onClick={() => navigate(-1)}
-                data-testid="Purge-cancel"
-            >
-                Purge
-            </Button>
-
-
-<form action="" method="post">
-              <button name="Purge" value="purge">Purge</button>
-            </form>
-
-
-
-
- <Button
-                type="submit"
-                data-testid="CollegiateSubredditForm-submit"
-            >
-                {buttonLabel}
-            </Button>
-
-
-
-
-
-<form action="" method="post">
-    <button name="foo" value="upvote">Upvote</button>
-</form>
-
-
-
-<Button
-                //variant="Secondary"
-                onClick={() => navigate(-1)}
-                data-testid="CollegiateSubredditForm-cancel"
-            >
-                Cancel
-            </Button>
-
-
-
-
-
-
-import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-
-export default function EarthquakesIndexPage() {
-  return (
-    <BasicLayout>
-      <div className="pt-2">
-        <h1>Earthquakes</h1>
-        <p>
-          This is where the index page will go
-        </p>
-      </div>
-    </BasicLayout>
-  )
-}
-*/
