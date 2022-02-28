f678b07363eed400c10fb0631161c80440a43ddb
rutvikjha <rutvikjha@umail.ucsb.edu>
2022-02-27 23:52:37 -0800

    rj - testing syntax issues

--- frontend/src/main/pages/Earthquakes/EarthquakesCreatePage.js
+++ frontend/src/main/pages/Earthquakes/EarthquakesCreatePage.js
@@ -13,7 +13,7 @@ export default function EarthquakesCreatePage() {
     method: "POST",
     params: {
       distance: earthquake.distance,
-      magnitude: earthquake.mag
+      magnitude: earthquake.minMag
     }
   });
 
